d64b801ecd1f8d890c854ec6916d7aa583b63d50
Frank Kung <kfanjian@gmail.com>
2018-01-16 23:29:07 +0800

    身份证信息

--- idcard.go
+++ idcard.go
@@ -2,7 +2,10 @@ package idcard
 
 import (
 	"strconv"
+	"time"
 	"unicode"
+
+	age "github.com/bearbin/go-age"
 )
 
 var weight = []int{7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2}
@@ -31,4 +34,29 @@ func Verify(id string) (ok bool) {
 		ok = true
 	}
 	return
+}
+
+func getBirthDay(id string) (time.Time, error) {
+	return time.Parse("20060102", id[6:14])
+}
+
+// IDCard 身份证信息
+type IDCard struct {
+	Valid    bool      `json:"valid"`
+	Birthday time.Time `json:"birthday"`
+}
+
+// Info 给出身份证上的信息
+func Info(id string) *IDCard {
+	var info = &IDCard{}
+
+	info.Valid = Verify(id)
+	info.Birthday, _ = getBirthDay(id)
+
+	return info
+}
+
+// Age 计算年龄
+func (i *IDCard) Age() int {
+	return age.Age(i.Birthday)
 }
